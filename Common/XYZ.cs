7d17770ab153967ff99b422beb40e67ca3158adb
zjf6738 <zjf6738@163.com>
2018-06-12 16:10:24 +0800

    增加XYZ到Common当中

---
+++ Common/XYZ.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Qzeim.ThrdPrint.BroadCast.Common
+{
+    public class XYZ
+    {
+        public XYZ(double _x, double _y, double _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+        private double x = 0;
+        private double y = 0;
+        private double z = 0;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public void SetXYZ(double _x, double _y, double _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+    }
+}
